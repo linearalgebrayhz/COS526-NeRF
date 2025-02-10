3bedba5a44a925429979c5bd66ca8933a3f1ed5a
Yang Haozhe <yhz2004@connect.hku.hk>
2025-02-11 07:44:38 +0800

    adding commeents to nerf module

--- nerf.py
+++ nerf.py
@@ -9,12 +9,26 @@ from encoder import PositionalEncoder
 # Model
 class NeRF(nn.Module):
     def __init__(self, D=8, W=256, output_ch=4, skips=[4], embedder=PositionalEncoder(10,0), embedder_dirs=None, use_viewdirs=False, input_ch=3, input_ch_views=3):
-        """
+        """_summary_
+
+        Args:
+            D (int, optional): Depth of the MLP. Defaults to 8.
+            W (int, optional): Width of each MLP Layer. Defaults to 256.
+            output_ch (int, optional): Output channels (RGB+density). Defaults to 4.
+            skips (list, optional): Layers where skip connections are added. Defaults to [4].
+            embedder (_type_, optional): Positional encoder for 3D coordinates. Defaults to PositionalEncoder(10,0).
+            embedder_dirs (_type_, optional): Positional encoder for viewing directions. Defaults to None.
+            use_viewdirs (bool, optional): Whether to use view directions. Defaults to False.
+            input_ch (int, optional): Input dimension for 3D coordinates. Defaults to 3.
+            input_ch_views (int, optional): Input dimension for viewing directions. Defaults to 3.
         """
         super(NeRF, self).__init__()
+        # Adjusts input_ch based on the positional encoder's output dimension.
         if embedder is not None:
             input_ch=embedder.out_dim
-
+        # Sets input_ch_views to 0 if view directions are not used.
+        # embedder_dirs us a separate positional encoder for view directions
+        # Eables view dependent effects like specular highlights
         if use_viewdirs:
             if embedder_dirs is None:
                 input_ch_views = input_ch_views
@@ -25,7 +39,7 @@ class NeRF(nn.Module):
 
         self.D = D
         self.W = W
-        self.input_ch = input_ch
+        self.input_ch = input_ch # after positional encoding: 3 + 3*2*L
         self.input_ch_views = input_ch_views
         self.skips = skips
         self.use_viewdirs = use_viewdirs
@@ -42,10 +56,16 @@ class NeRF(nn.Module):
         #------------------------------------------------------
         
         # assert False, 'Please finish the code before removing this assertion'
-
-        self.pts_linears = nn.ModuleList([nn.Linear(input_ch, W)] + [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
+        
+        # first layer + subsequent D-1 layers
+        # For layers in `skip`, add skip connection with original input. The dimension becomes W + input_ch
+        self.pts_linears = nn.ModuleList(
+            [nn.Linear(input_ch, W)] + 
+            [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
 
         ### Implementation according to the official code release (https://github.com/bmild/nerf/blob/master/run_nerf_helpers.py#L104-L105)
+        # combines spatial and view-direction info
+        # compre features to W//2
         self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W // 2)])
 
         ### Implementation according to the paper
@@ -59,6 +79,20 @@ class NeRF(nn.Module):
         else:
             self.output_linear = nn.Linear(W, output_ch)
 
+        # Architecture Flow for use_viewdirs = True
+        """
+        1. Spatial MLP (pts_linears):
+
+            Input: Positionally encoded 3D coordinates (input_ch).
+            Output: W-dimensional features + density (alpha).
+            
+        2. View-Dependent Branch:
+        
+            concatenate spatial features with encoded view directions
+            view_linears: (W + input_ch_views) → W//2
+            rgb_linear: W//2 → 3 (RGB)
+        
+        """
     def forward(self, inputs, viewdirs, netchunk=1024 * 64):
         """
         Forward pass of inputs and viewing directions through the nerf model
@@ -79,6 +113,7 @@ class NeRF(nn.Module):
             embedded = torch.cat([embedded, embedded_dirs], -1)
 
         out = []
+        # Netchunk is used here to avoid memory overflow
         for i in range(0, embedded.shape[0], netchunk):
             x = embedded[i:i + netchunk]
 
@@ -111,6 +146,7 @@ class NeRF(nn.Module):
         outputs = torch.reshape(out_flat, list(inputs.shape[:-1]) + [out_flat.shape[-1]])
         return outputs
 
+    # Loads pretrained weights from the original NeRF implementation (Keras) into PyTorch layers.
     def load_weights_from_keras(self, weights):
         assert self.use_viewdirs, "Not implemented if use_viewdirs=False"
 
